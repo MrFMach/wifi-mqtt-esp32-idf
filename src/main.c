5828ad112863c1da2c05880bfcd3a126f92a57aa
MrFMach <mrfmach@gmail.com>
2020-10-18 10:37:01 -0300

    code update

--- src/main.c
+++ src/main.c
@@ -3,53 +3,41 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
 #include "esp_system.h"
-#include "esp_event_loop.h"
 #include "lwip/sockets.h"
 #include "esp_wifi.h"
 #include "esp_log.h"
 #include "esp_event.h"
 #include "nvs_flash.h"
 
-#define SSID "Yahweh_2.4G"
-#define SSID_PASS "f@b100000"
+#define SSID "wifi ssid"
+#define SSID_PASS "wifi password"
 
 #define HTTP_SERVER_IP "93.184.216.34" // example.com
 #define HTTP_SERVER_PORT 80
 
 static const char *REQUEST_MESSAGE = "GET / HTTP/1.0\r\n"
-            "Host: example.com\r\n"
-            "User-Agent: ESP32 WiFi HTTP Client\r\n"
-            "\r\n";
-
-
-
-/* Variaveis e constantes - wi-fi */
-/* Coloque aqui o nome da rede wi-fi ao qual o ESP32 precisa se conectar */
-const char* ssid_wifi = " ";
- 
-/* Coloque aqui a senha da rede wi-fi ao qual o ESP32 precisa se conectar */
-const char* password_wifi = " ";
- 
-
-
-
+                                     "Host: example.com\r\n"
+                                     "User-Agent: ESP32 WiFi HTTP Client\r\n"
+                                     "\r\n";
 
 static const char *TAG = "scan";
 
 void requestHTTP(void);
 
-esp_err_t event_handler(void *ctx, system_event_t *event) {
+esp_err_t event_handler(void *ctx, system_event_t *event)
+{
 
-    if (event->event_id == SYSTEM_EVENT_STA_GOT_IP) {
+    if (event->event_id == SYSTEM_EVENT_STA_GOT_IP)
+    {
 
-        printf("IP recebido do AccessPoint: " IPSTR "\n",\ 
-                IP2STR(&event->event_info.got_ip.ip_info.ip));
+        printf("IP recebido do AccessPoint: %d.%d.%d.%d \n", IP2STR(&event->event_info.got_ip.ip_info.ip));
         requestHTTP();
-    } 
+    }
     return ESP_OK;
 }
 
-void requestHTTP() {
+void requestHTTP()
+{
 
     int sock, ret;
     struct sockaddr_in addr;
@@ -61,13 +49,15 @@ void requestHTTP() {
     addr.sin_addr.s_addr = inet_addr(HTTP_SERVER_IP);
 
     sock = lwip_socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
+    if (sock < 0)
+    {
         printf("sock < 0 - FATAL!!!\n");
         esp_restart();
     }
 
-    ret = lwip_connect(sock, (struct sockaddr*)&addr, sizeof(addr));
-    if (ret != 0) {
+    ret = lwip_connect(sock, (struct sockaddr *)&addr, sizeof(addr));
+    if (ret != 0)
+    {
         printf("connect != 0 - FATAL!!!\n");
         esp_restart();
     }
@@ -76,39 +66,15 @@ void requestHTTP() {
     lwip_send(sock, REQUEST_MESSAGE, strlen(REQUEST_MESSAGE), 0);
 
     char recvbuf[1024] = {0};
- 
-    while ((ret = lwip_recv(sock, recvbuf, 1024, 0)) > 0) {
+
+    while ((ret = lwip_recv(sock, recvbuf, 1024, 0)) > 0)
+    {
         printf("bytes recebidos: %d\n", ret);
         recvbuf[ret] = 0;
-        printf("conteudo recebido: %s\n", recvbuf); 
+        printf("conteudo recebido: %s\n", recvbuf);
     }
 
     lwip_close(sock);
-}
-
-
-void wifi_connect() {
-
-    printf("Inicializando cliente WiFi/HTTP...\n");
-
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
-    wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&config));
-    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
- 
-    wifi_config_t sta_config = {
-        .sta = {
-            .ssid = SSID,
-            .password = SSID_PASS,
-            .bssid_set = 0
-        }
-    };
-
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &sta_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-    ESP_ERROR_CHECK(esp_wifi_connect());
 }
 
 static void print_auth_mode(int authmode)
@@ -228,18 +194,17 @@ static void wifi_scan(void)
     uma mensagem de erro será impressa no console 
     e abort()será chamada
     */
-   
+
     ESP_ERROR_CHECK(esp_netif_init()); //Inicializa a pilha TCP/IP e retorna ESP_OK ou ESP_FAIL
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
     assert(sta_netif);
 
-
     //configuração do Wifi com os valores padrão
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    uint16_t scanlist = 5; // maximum number of scan list
+    uint16_t scanlist = 3; // maximum number of scan list
     wifi_ap_record_t ap_info[scanlist];
     uint16_t ap_count = 0;
     memset(ap_info, 0, sizeof(ap_info));
@@ -271,14 +236,6 @@ static void wifi_scan(void)
         }
         ESP_LOGI(TAG, "Channel \t\t%d\n", ap_info[i].primary);
     }
-
-    esp_err_t esp_wifi_set_config(wifi_interface_t interface, wifi_config_t *conf);
-
-    esp_err_t esp_wifi_disconnect(void);
-
-    esp_err_t esp_wifi_connect(void);
-
-
 }
 
 void app_main(void)
@@ -291,8 +248,25 @@ void app_main(void)
     printf("\n --- wifi_scan() --- \n\v");
     wifi_scan();
 
-    printf("\n --- wifi_connect() --- \n\v");    
-    wifi_connect();
+    printf("\n --- wifi_connect() --- \n\v");
+        printf("Inicializando cliente WiFi/HTTP...\n");
 
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+    wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&config));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+ 
+    wifi_config_t sta_config = {
+        .sta = {
+            .ssid = SSID,
+            .password = SSID_PASS,
+            .bssid_set = 0
+        }
+    };
 
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &sta_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_connect());
 }
